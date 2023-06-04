332b01d391b58262a4278f4e86130daa5e99f67b
Havish <Havish747@gmail.com>
2023-06-04 14:45:57 -0400

    Config now a struct, proper error handeling added

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,35 @@
 use std::env;
 use std::fs;
+use std::process;
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn builder(args: Vec<String>) -> Result<Config, String> {
+        if args.len() < 3 {
+            let err = format!("Expected 2 arguments, got {}", args.len() - 1);
+            return Err(err);
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        return Ok(Config { query, file_path });
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect::<Vec<String>>();
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::builder(args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {} in {}", query, file_path);
+    println!("Searching for {} in {}", config.query, config.file_path);
     let file_content =
-        fs::read_to_string(file_path).expect("Something went wrong reading the file");
+        fs::read_to_string(config.file_path).expect("Something went wrong reading the file");
 
     println!("File text:\n{file_content}");
 }
