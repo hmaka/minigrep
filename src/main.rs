e54f8e45d5b4fbe7eab027e75164351115338323
Havish <Havish747@gmail.com>
2023-06-03 23:20:50 -0400

    Added basic functionality, needs refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use std::env;
-
+use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect::<Vec<String>>();
-    dbg!(&args);
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {} in {}", query, file_path);
+    let file_content =
+        fs::read_to_string(file_path).expect("Something went wrong reading the file");
+
+    println!("File text:\n{file_content}");
 }
